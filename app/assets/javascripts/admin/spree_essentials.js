ed1a491352969a1e6b684d430dfb1ecafa21c551
Sam Beam <sbeam@onsetcorps.net>
2013-01-25 15:53:48 -0500

    replace markdown editor with tinymce by popular request (ugh :/)

--- app/assets/javascripts/admin/spree_essentials.js
+++ app/assets/javascripts/admin/spree_essentials.js
@@ -1,1 +1,5 @@
-//= require_tree .
+//= require ./jquery-migrate-1.0.0.js
+//= require ./tiny_mce/tiny_mce.js
+//= require ./tiny_mce/jquery.tinymce.js
+//= require ./jquery.autodate.js
+//= require ./date.js
